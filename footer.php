a148f5d299ea701fef00912b88cd527e0bb7ee9a
Julio Aenlle <gmg8r251@ufl.edu>
2016-10-30 22:32:21 -0400

    Add footer code to footer.php

--- footer.php
+++ footer.php
@@ -11,16 +11,32 @@
 
 ?>
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'thig_cat_5' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'thig_cat_5' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'thig_cat_5' ), 'thig_cat_5', '<a href="http://underscores.me/" rel="designer">Category 5 Creative</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+    </div><!-- #content -->
+        <footer class="site-footer" role="contentinfo">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-sm-4 col-xs-6">
+                        <?php wp_nav_menu( array( 'theme_location' => 'footercompany-menu', 'menu_id' => 'footer_company' ) ); ?>
+                    </div>
+                    <div class="col-md-4 col-sm-4 col-xs-6">
+                        <?php wp_nav_menu( array( 'theme_location' => 'footerquicklinks-menu', 'menu_id' => 'footer_quick_links' ) ); ?>
+                    </div>
+                    <div class="col-md-4 col-sm-4">
+                        <a href="http://www.julioaenlle.net/thig/">
+                            <img id="footerlogoimg" src="http://localhost/thig/wp-content/uploads/2016/10/thig_all_white.png" alt="Tower Hill Insurance | Florida Homeowners Insurance" />
+                        </a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12 pagination-centered">
+                        <p>
+                            &copy; Copyright 2016 Tower Hill Insurance Group
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
 
 <?php wp_footer(); ?>
 
