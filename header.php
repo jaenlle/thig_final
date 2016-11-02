d5517328342267284b62fe92f458689d49d42256
Julio Aenlle <gmg8r251@ufl.edu>
2016-11-01 23:43:34 -0400

    Add title tag to allow change of title on wpdash

--- header.php
+++ header.php
@@ -16,6 +16,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link href="https://fonts.googleapis.com/css?family=Lato:700,700i,900|Open+Sans:600" rel="stylesheet">
+<title>Tower Hill Insurance | Florida Homeowners Insurance</title>
 
 <?php wp_head(); ?>
 </head>
